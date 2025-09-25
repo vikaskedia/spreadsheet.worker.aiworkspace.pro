1dc70aeac366a28bea43af984223ced781212cfe
tj <tanmoyjana@grmtech.com>
2025-09-25 19:29:45 +0530

    depoyed wrangler

--- spreadsheet-wroker/src/index.js
+++ spreadsheet-wroker/src/index.js
@@ -10,6 +10,6 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World from spreadsheet-worker test!');
+		return new Response('Hello World from spreadsheet-worker!');
 	},
 };
