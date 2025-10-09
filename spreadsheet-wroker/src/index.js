af9cc07a918c504c3b2ea4e235aadc82ef398df7
tj <tanmoyjana@grmtech.com>
2025-10-09 17:01:49 +0530

    updated spreadsheet cron for sc ops

--- spreadsheet-wroker/src/index.js
+++ spreadsheet-wroker/src/index.js
@@ -43,8 +43,8 @@ async function handleSpreadsheetUpdateCron(env) {
   //const apiUrl = "https://spreadsheet.aiworkspace.pro/api/update-spreadsheet?workspace_id=686&portfolio_id=portfolio_1754496102288_yzut7nhkn";
   const apiUrl = "https://spreadsheet.aiworkspace.pro/api/update-spreadsheet";
   const params = new URLSearchParams({
-	workspace_id: "686",
-	portfolio_id: "portfolio_1754496102288_yzut7nhkn"
+	workspace_id: "309", //"686",
+	portfolio_id: "portfolio_95_1753135771.217412", // "portfolio_1754496102288_yzut7nhkn"
   });
   
   const response = await fetch(`${apiUrl}?${params.toString()}`, {
